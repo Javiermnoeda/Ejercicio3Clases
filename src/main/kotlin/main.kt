913baa7e51b57d20e46ac90fdf5c247e3ec4a3f0
Javier <jmnoeda2000@gmail.com>
2021-04-21 13:21:39 +0200

    Cambios en Clase

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,29 @@
 fun main(){
+
     val nombreCompleto = NombreCompleto("Carlos","Tena")
     val nombreCompleto2 = NombreCompleto("Javier","Martin")
 
-    val nombreMascota = NombreMascota("Moway")
-    val nombreMascota2 = NombreMascota("Coco")
-    val mascota = Mascota(nombreMascota, "Europeo Común",12)
-    val mascota2 = Mascota2(nombreMascota2,"Siamés",15)
+
+
+    val mascota = Mascota("Moway", "Europeo Común",12)
+    val mascota2 = Mascota("Coco","Siamés",15)
 
     mascota.setchip(123)
     mascota2.setchip(345)
 
-    val persona = Persona(nombreCompleto,nombreMascota, nombreMascota2, mascota, mascota2)
-    val persona2 = Persona(nombreCompleto2,nombreMascota, nombreMascota2, mascota=null,mascota2=null)
+    val persona = Persona(nombreCompleto, mascota, mascota2)
+    val persona2 = Persona(nombreCompleto2,mascota=null,mascota2=null)
 
     persona.edad=39
     persona2.edad=20
     persona.setDni("12345678X")
     persona2.setDni("87654321Z")
 
-    persona.tieneMascota()
-    persona2.tieneMascota()
-
-
+    persona.escribeMascota()
+    persona2.escribeMascota()
 }
 
-class Persona(var nombreCompleto : NombreCompleto,var nombreMascota: NombreMascota, var nombreMascota2: NombreMascota,  var mascota: Mascota? = null, var mascota2: Mascota2? = null){
+class Persona(var nombreCompleto : NombreCompleto, var mascota: Mascota? = null, var mascota2: Mascota? = null){
     // edad es publica, no pongo restricciones a que lo cambien
     var edad = 18
     private var DNI : String? = null
@@ -37,20 +36,25 @@ class Persona(var nombreCompleto : NombreCompleto,var nombreMascota: NombreMasco
         }
     }
 
-    fun tieneMascota(){ //Sobreescribe el metodo String
-        if (mascota != null && mascota2 != null) {
-            println("${nombreCompleto.nombre} con DNI $DNI y soy dueño de ${nombreMascota.nombre} y ${nombreMascota2.nombre}")
-        } else {
-            if(mascota != null && mascota2 == null){
-                println("${nombreCompleto.nombre} con DNI $DNI y soy dueño de ${nombreMascota.nombre}")
-            } else {
-                if(mascota == null && mascota2 != null){
-                    println("${nombreCompleto.nombre} con DNI $DNI y soy dueño de ${nombreMascota2.nombre}")
-                } else {
-                    println("${nombreCompleto.nombre} con DNI $DNI y no tengo mascota")
-                }
-            }
+    fun escribeMascota(){ //Sobreescribe el metodo String
+
+        print("Soy ${nombreCompleto.nombre} con DNI $DNI")
+
+        if (mascota==null) {
+            mascota = mascota2
+            mascota2 = null
         }
+
+        mascota?.let{
+            print(" y soy dueño de ${it.nombreMascota}")
+        } ?: run{
+            print(" y no tengo mascota")
+        }
+
+        mascota2?.let {
+            print("y soy dueño de ${it.nombreMascota}")
+        }
+        println("")
     }
 }
 
@@ -62,15 +66,7 @@ class NombreMascota( var nombre: String){
 
 }
 
-class Mascota (var nombreMascota: NombreMascota,var raza: String, var edadMascota: Int){
-    private var numeroChip : Int? = null
-
-    fun setchip (numeroChip: Int){
-        this.numeroChip = numeroChip
-    }
-}
-
-class Mascota2 (var nombreMascota2: NombreMascota, var raza: String, var edadMascota: Int){
+class Mascota (var nombreMascota: String,var raza: String, var edadMascota: Int){
     private var numeroChip : Int? = null
 
     fun setchip (numeroChip: Int){
